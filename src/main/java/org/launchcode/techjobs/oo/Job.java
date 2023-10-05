5e3f652f1f7835adadf37731d7319907fa9e8876
Andrew Lanning <116489166+andrewlanning@users.noreply.github.com>
2023-10-05 00:07:50 -0500

    Update Job.java

--- src/main/java/org/launchcode/techjobs/oo/Job.java
+++ src/main/java/org/launchcode/techjobs/oo/Job.java
@@ -13,13 +13,105 @@ public class Job {
     private PositionType positionType;
     private CoreCompetency coreCompetency;
 
-    // TODO: Add two constructors - one to initialize a unique ID and a second to initialize the
-    //  other five fields. The second constructor should also call the first in order to initialize
-    //  the 'id' field.
+    //TODO: COMPLETE
+    public Job() {
+        id = nextId;
+        nextId++;
+    }
 
-    // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
-    //  match.
+    public Job(String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency) {
+        this();
+        this.name = name;
+        this.employer = employer;
+        this.location = location;
+        this.positionType = positionType;
+        this.coreCompetency = coreCompetency;
+    }
 
-    // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID
-    //  and id.
+    //TODO: COMPLETE
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Employer getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(Employer employer) {
+        this.employer = employer;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public PositionType getPositionType() {
+        return positionType;
+    }
+
+    public void setPositionType(PositionType positionType) {
+        this.positionType = positionType;
+    }
+
+    public CoreCompetency getCoreCompetency() {
+        return coreCompetency;
+    }
+
+    public void setCoreCompetency(CoreCompetency coreCompetency) {
+        this.coreCompetency = coreCompetency;
+    }
+
+    //TODO: COMPLETE
+    @Override
+    public String toString() {
+        if (getName() == null &&
+                getEmployer() == null &&
+                getLocation() == null &&
+                getPositionType() == null &&
+                getCoreCompetency() == null) {
+            return "OOPS! This job does not seem to exist.";
+        }
+
+        String idStr = "ID: " + getId();
+        String nameStr = "Name: " + (getName() != null && !getName().isEmpty() ? getName() : "Data not available");
+        String employerStr = "Employer: " + (getEmployer() != null && !getEmployer().getValue().isEmpty() ? getEmployer().getValue() : "Data not available");
+        String locationStr = "Location: " + (getLocation() != null && !getLocation().getValue().isEmpty() ? getLocation().getValue() : "Data not available");
+        String positionTypeStr = "Position Type: " + (getPositionType() != null && !getPositionType().getValue().isEmpty() ? getPositionType().getValue() : "Data not available");
+        String coreCompetencyStr = "Core Competency: " + (getCoreCompetency() != null && !getCoreCompetency().getValue().isEmpty() ? getCoreCompetency().getValue() : "Data not available");
+
+        //TODO: ALTERNATIVELY COULD USE System.lineSeparator(), CURRENTLY USING "\n" TO PASS TESTS
+        return System.lineSeparator() + idStr +
+                System.lineSeparator() + nameStr +
+                System.lineSeparator() + employerStr +
+                System.lineSeparator() + locationStr +
+                System.lineSeparator() + positionTypeStr +
+                System.lineSeparator() + coreCompetencyStr +
+                System.lineSeparator();
+    }
+
+    //TODO: COMPLETE
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Job job = (Job) o;
+        return id == job.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
